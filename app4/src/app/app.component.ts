acb7f62551266e08053ca2671468341b67b68c16
Rajan Gauchan <hirajan10@hotmail.com>
2024-01-30 14:30:26 -0500

    Add pipes in the template (HTML) file  to transform data before display

--- app4/src/app/app.component.ts
+++ app4/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   // this.employee = new Employee(1,"Rajan",10000);
 
   // 2 way to instances 
- employee = new Employee(1,"Rajan",10000);
+ //employee = new Employee(1,"Rajan",10000);
 
  // 3 way to instances
  // employee var of type Employee
@@ -28,11 +28,11 @@ export class AppComponent {
 //     this.employee = new Employee(1, 'John Doe', 50000);
 
 // list of employee objects creation
-employees:Employee[]= [new Employee(101,"John",5000), 
-                       new Employee(102,"Jane",6000), 
-                       new Employee(103,"Jack",7000)
-                      ];
-newemployee: Employee= new Employee(0, "", 0);                  
+// employees:Employee[]= [new Employee(101,"John",5000), 
+//                        new Employee(102,"Jane",6000), 
+//                        new Employee(103,"Jack",7000)
+//                       ];
+// newemployee: Employee= new Employee(0, "", 0);                  
   /**
    * Inserts a new employee into the employees array by pushing a new Employee instance.
    * Resets the newemployee properties after inserting. 
@@ -40,20 +40,34 @@ newemployee: Employee= new Employee(0, "", 0);
    * Deletes an employee from the employees array by splicing the array at the given index.
    * Confirms deletion with the user first.
   */
-  onInsertClick(){
+  // onInsertClick(){
  
-    this.employees.push(new Employee(this.newemployee.empId, this.newemployee.empName, this.newemployee.salary));
+  //   this.employees.push(new Employee(this.newemployee.empId, this.newemployee.empName, this.newemployee.salary));
 
-    this.newemployee.empId=0;
-    this.newemployee.empName="";
-    this.newemployee.salary=0;
-  }
-    onDeleteClick(n: any){
-      if(confirm("Are you sure you want to delete this employee?")){
-        this.employees.splice(n,1);
-      }
-  }
-  }
+  //   this.newemployee.empId=0;
+  //   this.newemployee.empName="";
+  //   this.newemployee.salary=0;
+  // }
+  //   onDeleteClick(n: any){
+  //     if(confirm("Are you sure you want to delete this employee?")){
+  //       this.employees.splice(n,1);
+  //     }
+  // }
+
+  city:string ="Kathmandu";
+  salary: number=10000;
+  n:number=0.72;
+  dt : Date= new Date();
+
+  // we are creating an object person having structure firstName,lastName, age and assign values to it
+person: {firstName:string,lastName:string,age:number} ={firstName:"Rajan",lastName:"Kumar",age:20};  //typescript notation
+
+
+ 
+}
+
+
+  
     
   
 
